8ac98a03f4ddf3d9f52c313a73079f39b9890114
Francis Joseph <francis2241998@gmail.com>
2022-09-11 01:26:14 +0530

    Create inner_join.py

---
+++ inner_join.py
@@ -0,0 +1,9 @@
+class inner_join(beam.DoFn):
+  def process(self,ele):
+    data = ele[1]
+    #replace left, right string with respectice CoGroupByKey value
+    left,right= data['<left>'], data['<right>']
+    if left and right:
+      yield {**left[0],**right[0]}
+    else:
+      None
