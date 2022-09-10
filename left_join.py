ede36a1db0ee6de74537f8b27629247bf0765674
Francis Joseph <francis2241998@gmail.com>
2022-09-11 01:27:49 +0530

    Create left_join.py

---
+++ left_join.py
@@ -0,0 +1,14 @@
+class left_join(beam.DoFn):
+  def process(self, ele):
+    data = ele[1]
+    left,right= data['<left>'], data['<right>']
+    try:
+      if right:#notempty
+        for i in right[0]:
+          if i != '<JoinKey>':
+            left[0][i]=None    
+        yield left[0]
+      else:
+         None
+    except IndexError :
+      pass
